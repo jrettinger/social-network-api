c9f733e4b74ef0f4c4579d073105ba9b1a24ad44
unknown <jason.rettinger@gmail.com>
2022-10-11 15:19:19 -0400

    added routes

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const env = require("dotenv");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//ENVIRONMENT VARIABLE/CONSTANTS
+env.config();
+
+//MONGODB CONNECTION
+require("./db/connection");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use(require("./routes"));
+
+// Use this to log mongo queries being executed!
+mongoose.set("debug", true);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
