57fb9ab9d159f7d3bb889d94b25fdf78a7ba08df
unknown <jason.rettinger@gmail.com>
2022-10-12 18:47:41 -0400

    added to gitignore

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const env = require("dotenv");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 //ENVIRONMENT VARIABLE/CONSTANTS
 env.config();
